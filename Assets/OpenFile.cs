9bfd43059127968bc8520b72eaffc0e8d5a46fc9
hamakuk <kukai.beaudry@gmail.com>
2019-10-20 22:13:50 -0400

    Added File Selection UI

---
+++ Assets/OpenFile.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+using UnityEngine.WSA;
+using UnityEngine.UI;
+
+#if ENABLE_WINMD_SUPPORT
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Storage.Pickers;
+#endif
+
+public class OpenFile : MonoBehaviour
+{
+    public Text label;
+    public GameObject cube;
+
+#if ENABLE_WINMD_SUPPORT
+    private FileOpenPicker openPicker;
+#endif
+
+    public void SelectFile()
+    {
+        UnityEngine.Debug.LogFormat("UnityThread: {0}", Thread.CurrentThread.ManagedThreadId);
+
+#if ENABLE_WINMD_SUPPORT
+        UnityEngine.WSA.Application.InvokeOnUIThread(OpenFileAsync, false);  
+#else
+        UnityEngine.Debug.Log("ENABLE_WINMD_SUPPORT false");
+#endif
+    }
+
+    void Start()
+    {
+
+    }
+
+    void ThreadCallback()
+    {
+        UnityEngine.Debug.LogFormat("ThreadCallback() on thread: \t{0}", Thread.CurrentThread.ManagedThreadId);
+    }
+
+#if ENABLE_WINMD_SUPPORT
+    public async void OpenFileAsync()
+    {  
+        UnityEngine.Debug.LogFormat( "OpenFileAsync() on Thread: {0}", Thread.CurrentThread.ManagedThreadId );
+
+        openPicker = new FileOpenPicker();
+ 
+        //openPicker.ViewMode = PickerViewMode.Thumbnail;
+        //openPicker.SuggestedStartLocation = PickerLocationId.Objects3D;
+        //openPicker.FileTypeFilter.Add(".fbx");
+        openPicker.FileTypeFilter.Add("*");
+     
+        StorageFile file = await openPicker.PickSingleFileAsync();
+        string labelText = String.Empty;
+        if ( file != null )
+        {
+            // Application now has read/write access to the picked file 
+            labelText = "Picked file: " + file.DisplayName;
+        }
+        else
+        {
+            // The picker was dismissed with no selected file 
+            labelText = "File picker operation cancelled";
+        }
+        
+        UnityEngine.Debug.Log( labelText );
+
+        UnityEngine.WSA.Application.InvokeOnAppThread( ThreadCallback, false );
+
+        UnityEngine.WSA.Application.InvokeOnAppThread( new AppCallbackItem( () => { label.text = labelText; } ), false );
+    }
+#endif
+
+}

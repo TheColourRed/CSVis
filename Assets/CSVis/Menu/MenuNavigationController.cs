ac9bd88ded29200b0258255463acdd6cbf54bd65
hamakuk <kukai.beaudry@gmail.com>
2020-03-04 23:16:16 -0500

    adding generated meta files

---
+++ Assets/CSVis/Menu/MenuNavigationController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace CSVis.Menu
+{
+    public class MenuNavigationController : MonoBehaviour
+    {
+        public GameObject currentMenu;
+        
+        public GameObject previousMenu;
+        
+        public GameObject mainMenu;
+        
+        public GameObject minimizeMenu;
+
+        public GameObject closeButton;
+
+        public GameObject backButton;
+
+        public GameObject minimizeButton;
+        
+        /// <summary>
+        /// Function to map the close button to
+        /// Hides the current menu and return to the main menu
+        /// </summary>
+        private void OnClose()
+        {
+            Debug.Log("Close Button pressed");
+            mainMenu.SetActive(true);
+            currentMenu.SetActive(false);
+        }
+
+        /// <summary>
+        /// Function to map the back button to
+        /// Hides the current menu and display previous menu
+        /// </summary>
+        private void OnBack()
+        {
+            Debug.Log("Back Button pressed");
+            previousMenu.SetActive(true);
+            currentMenu.SetActive(false);
+        }
+        
+        /// <summary>
+        /// Function to map the minimize button to
+        /// Hides the current menu and display minimized menu
+        /// </summary>
+        private void OnMinimize()
+        {
+            Debug.Log("Minimize Button pressed");
+            Instantiate(minimizeMenu);
+            currentMenu.SetActive(false);
+        }
+        
+        /// <summary>
+        /// Initialize event handlers for button clicks at run time
+        /// as well as set the return menu for the minimized menu  
+        /// </summary>
+        private void Start()
+        {
+            minimizeMenu.GetComponent<MinimizedMenuController>().returnMenu = currentMenu;
+            
+            var close = closeButton.GetComponent<Interactable>();
+            var back = backButton.GetComponent<Interactable>();
+            var minimize = minimizeButton.GetComponent<Interactable>();
+
+            close.OnClick.AddListener(OnClose);
+            back.OnClick.AddListener(OnBack);
+            minimize.OnClick.AddListener(OnMinimize);
+        }
+
+    }
+}
---
+++ Assets/CSVis/Menu/MinimizedMenuController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using UnityEngine;
+
+namespace CSVis.Menu
+{
+    public class MinimizedMenuController : MonoBehaviour
+    {
+        public GameObject maximizeButton;
+        
+        public GameObject returnMenu;
+        
+        /// <summary>
+        /// Function to map the maximize button to
+        /// Hides the minimize menu and display the previous menu
+        /// </summary>
+        private void OnMaximize()
+        {
+            Debug.Log("Maximize Button pressed");
+            returnMenu.SetActive(true);
+            Destroy(gameObject);
+        }
+        
+        /// <summary>
+        /// Initialize event handlers for button clicks at run time 
+        /// </summary>
+        private void Start()
+        {
+            var maximize = maximizeButton.GetComponent<Interactable>();
+            
+            maximize.OnClick.AddListener(OnMaximize);
+        }
+    }
+}
